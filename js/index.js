9075c14a118ed07b265c1f4915e88f8b6861644a
jerry <17app001@gmail.com>
2022-01-06 15:31:09 +0800

    tabs update & javascript switch context

--- js/index.js
+++ js/index.js
@@ -6,3 +6,24 @@ const navbarMenuEl = document.querySelector("#nav-links");
 burgerEl.addEventListener("click", () => {
     navbarMenuEl.classList.toggle('is-active');
 });
+
+//tab
+const tabEls = document.querySelectorAll('.tabs li');
+const tabContentEls = document.querySelectorAll('#tab-content>div');
+
+tabEls.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        // 移除活動模式
+        tabEls.forEach(item => item.classList.remove('is-active'));
+        tab.classList.add('is-active');
+        // 判斷顯示哪個內容
+        const target = tab.dataset.target;
+        tabContentEls.forEach(box => {
+            if (box.getAttribute('id') == target) {
+                box.classList.remove('is-hidden');
+            } else {
+                box.classList.add('is-hidden');
+            }
+        });
+    });
+});
